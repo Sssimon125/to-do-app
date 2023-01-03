8ee79e2cbc5ed9808593cee829a55b18a3ae1220
ardit <simonyuno25@gmail.com>
2023-01-03 13:20:14 -0800

    Merged changes

--- app/graph.py
+++ app/graph.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 
 
