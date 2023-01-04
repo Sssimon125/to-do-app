565af4eace26658e4e64e9faffeeee04e994fcce
Simon Yunoshev <simonyuno25@gmail.com>
2023-01-04 11:30:45 -0800

    Small changes at add button

--- app/graph.py
+++ app/graph.py
@@ -7,7 +7,8 @@ sg.theme("Black")
 clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add", size=10)
+add_button = sg.Button("Add", size=10,mouseover_colors="LightBlue2",
+                       tooltip="Add a todo")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
